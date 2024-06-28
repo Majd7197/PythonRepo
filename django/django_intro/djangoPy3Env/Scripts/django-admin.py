144ecb17d19d5ebec0e06abead1a16d28c4aefeb
Majd <abusadamajd@gmail.com>
2024-06-28 23:20:50 +0300

    test

---
+++ django/django_intro/djangoPy3Env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Majd.DESKTOP-898IICL\Desktop\git\demoRepo\django\django_intro\djangoPy3Env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
