d27307900fd82765e280fc02687e4dc687cf769d
Majd Abu Sada <abusadamajd@gmail.com>
2024-07-11 15:54:30 +0300

    Users_shell_assignment

--- django/users_shell/users_shell_app/models.py
+++ django/users_shell/users_shell_app/models.py
@@ -1,14 +1,12 @@
 from django.db import models
-    
-class Movie(models.Model):
-    title = models.CharField(max_length=45)
-    description = models.TextField()
-    release_date = models.DateTimeField()
-    duration = models.IntegerField()
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email_address = models.CharField(max_length=255)
+    age = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-class Movie(models.Model):
-    # fields removed for brevity
+                                      
     def __str__(self):
-        return f"<Movie object: {self.title} ({self.id})>"
+        return f"<User object: name:{self.first_name}{self.last_name} email:{self.email_address} age:{self.age} created at: {self.created_at} updated at:{self.updated_at}>"
