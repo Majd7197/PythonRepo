d31ea7e298933de0449f6a59af01637a0c072385
Majd Abu Sada <abusadamajd@gmail.com>
2024-07-16 16:14:46 +0300

    books_author_with templeates app not completed 16-7-24

--- django/books_authors_with_templates/books_authors_with_templates_app/urls.py
+++ django/books_authors_with_templates/books_authors_with_templates_app/urls.py
@@ -5,4 +5,10 @@ urlpatterns = [
    # path('admin/', admin.site.urls),
    path('',views.index),
    path('add_book',views.add_book),
+   path('books/<int:id>/',views.get_book),
+   path('authors',views.display_authors),
+   path('add_author',views.add_author),
+   path('author/<int:id>/',views.get_author,name = 'author_details'),
+   path('add_book_to_author', views.add_book_to_author, name='add_book_to_author'),
+   path('add_author_to_book', views.add_author_to_book, name='add_author_to_book'),
 ]
--- django/books_authors_with_templates/books_authors_with_templates_app/views.py
+++ django/books_authors_with_templates/books_authors_with_templates_app/views.py
@@ -8,10 +8,57 @@ def index(request):
     }
     return render(request,'index.html',context)
 
+def display_authors(request):
+    context = {
+        'books' : Book.objects.all(),
+        'authors' : Author.objects.all()
+    }
+    return render(request,'display_authors.html',context)
+
 def add_book(request):
     if request.method == "POST":
         title_from_form = request.POST.get('title')
         description_from_form = request.POST.get('description')
         Book.objects.create(title = title_from_form, desc = description_from_form)
     return redirect('/')
-# Create your views here.
+def add_author(request):
+    if request.method == "POST":
+        first_name_from_form = request.POST.get('first_name')
+        last_name_from_form = request.POST.get('last_name')
+        notes_from_form = request.POST.get('notes')
+        Author.objects.create(first_name = first_name_from_form, last_name = last_name_from_form, notes = notes_from_form)
+    return redirect('/authors')
+def get_book(request,id):
+    book = Book.objects.get(id=id)
+    context = {
+        'book' : book
+    }
+    return render(request,'book_details.html',context)
+def get_author(request,id):
+    author = Author.objects.get(id=id)
+    books = Book.objects.all()
+    context = {
+        'author' : author,
+        'books' : books,
+    }
+    return render(request,'author_details.html',context)
+
+def add_book_to_author(request):
+    if request.method == "POST":
+        book_id = request.POST.get('book')
+        author_id = request.POST.get('author_id')
+        book = Book.objects.get(id=book_id)
+        author = Author.objects.get(id=author_id)
+        author.books.add(book)
+        return redirect(f'/author/{author.id}/')
+    
+def add_author_to_book(request):
+    if request.method == "POST":
+        book_id = request.POST.get('book')
+        author_id = request.POST.get('author_id')
+        book = Book.objects.get(id=book_id)
+        author = Author.objects.get(id=author_id)
+        
+        # Add the book to the author's collection of books
+        book.author.add(book)
+        return redirect(f'/author/{book.id}/')
