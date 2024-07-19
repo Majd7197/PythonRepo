863f19c5dc82a44a0be55df360d27dde4c9c383e
Majd <abusadamajd@gmail.com>
2024-07-19 20:39:11 +0300

    semi restful tv shows completed

--- django/semi_restful_tv_shows/semi_restful_tv_shows_app/urls.py
+++ django/semi_restful_tv_shows/semi_restful_tv_shows_app/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('shows',views.shows),
     path('add_show',views.add_show),
     path('shows/<int:id>/',views.show_info),
+    path('shows/<int:id>/destroy/',views.delete_show),
+    path('shows/<int:id>/edit',views.edit_show_page),
+    path('edit_show',views.edit_show),
+    
 ]
--- django/semi_restful_tv_shows/semi_restful_tv_shows_app/views.py
+++ django/semi_restful_tv_shows/semi_restful_tv_shows_app/views.py
@@ -26,4 +26,28 @@ def show_info(request,id):
         'show':show
     }
     return render(request,'show_info.html',context)
-        
+def delete_show(request,id):
+    show = Show.objects.get(id=id)
+    show.delete()
+    return redirect('/shows')
+def edit_show_page(request,id):
+    show = Show.objects.get(id=id)
+    context = {
+        'show' : show
+    }
+    
+    return render(request,"edit_show.html",context)
+
+def edit_show(request):
+    show_id_from_form = request.POST.get('id_show')
+    show = Show.objects.get(id=show_id_from_form)
+    updated_title_from_form = request.POST.get('title')
+    updated_network_from_form = request.POST.get('network')
+    updated_release_date_from_form = request.POST.get('release_date')
+    updated_description_from_form = request.POST.get('description') 
+    show.title = updated_title_from_form
+    show.network = updated_network_from_form
+    show.release_date = updated_release_date_from_form
+    show.description = updated_description_from_form
+    show.save()
+    return redirect(f'/shows/{show_id_from_form}/')
