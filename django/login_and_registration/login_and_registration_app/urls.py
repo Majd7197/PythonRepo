36439a5bfb9c415c1d3a477bf91fb1180174b6bc
Majd Abu Sada <abusadamajd@gmail.com>
2024-07-22 16:39:07 +0300

    login and registration not completed two

--- django/login_and_registration/login_and_registration_app/urls.py
+++ django/login_and_registration/login_and_registration_app/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path,include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index)
+    path('',views.index),
+    path('registration',views.index),
+    path('login',views.login),
+    path('success',views.success)
 ]
--- django/login_and_registration/login_and_registration_app/views.py
+++ django/login_and_registration/login_and_registration_app/views.py
@@ -25,3 +25,31 @@ def index(request):
             return redirect('/')
     return render(request, 'index.html')
 
+def login(request):
+    if request.method == "POST":
+        # See if the email provided exists in the database
+        users = User.objects.filter(email=request.POST['email'])  # Using filter
+
+        if users:  # Check if any user was found
+            for user in users:
+                # Use bcrypt's checkpw method, passing the hash from our database and the password from the form
+                if bcrypt.checkpw(request.POST['password'].encode(), user.password.encode()):
+                    # If we get True after checking the password, we may put the user id in session
+                    request.session['userid'] = user.id
+                    request.session['first_name'] = user.first_name
+                    request.session['last_name'] = user.last_name
+                    # Never render on a post, always redirect!
+                    return redirect('/success')
+            # If no password matched
+            messages.error(request, "Incorrect password")
+        else:
+            # Handle the case where no users were found
+            messages.error(request, "User does not exist")
+        
+        return redirect("/")
+
+    # If request is not POST, render login form (assuming you have a login form)
+    return render(request, 'login.html')
+
+def success(request):
+    return render(request,'success.html')
